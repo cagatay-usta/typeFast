3606945c0a06df9126e3d8fd538045f5b39dad99
cagatay-usta <cagatay_usta@windowslive.com>
2023-03-24 00:07:38 +0300

    all pages basic designs and routes created

--- app.py
+++ app.py
@@ -4,17 +4,27 @@ import sqlite3
 
 app = Flask(__name__)
 
+
 # connect to sqlite database
 con = sqlite3.connect("typefast.db")
 db = con.cursor()
+
 
 # main route 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
         return render_template("index.html")
+    if request.method == 'POST':
+        return render_template("index.html") # when case selection submitted change the text to type
+    
+@app.route("/leaderboard", methods=["GET", "POST"])
+def leaderboard():
+    if request.method == "GET":
+        return render_template("leaderboard.html")
     
 
 
 # close connection to database when done 
 con.close()
+
