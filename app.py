db06e7c81eea12ebf88604844ea62201a7a34023
cagatay-usta <cagatay_usta@windowslive.com>
2023-03-25 01:20:31 +0300

    case functions added

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import sqlite3
 import csv
 from helper_functions import word_generator
 
+
 app = Flask(__name__)
 
 
@@ -14,10 +15,6 @@ db = con.cursor()
 # store the words to memory from the csv
 with open("500-words.csv") as file:
     words = [word.rstrip() for word in file]
-
-
-
-
 
 
 # main route 
@@ -32,7 +29,6 @@ def index():
 def leaderboard():
     if request.method == "GET":
         return render_template("leaderboard.html")
-    
 
 
 # close connection to database when done 
